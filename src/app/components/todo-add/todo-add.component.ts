7b14af446cdbdf3b826c46108740a238179f6e0e
Vietstars <vietstar.nt@gmail.com>
2020-03-11 20:06:44 +0700

    update [attr.checked] for checkbox

--- src/app/components/todo-add/todo-add.component.ts
+++ src/app/components/todo-add/todo-add.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 })
 export class TodoAddComponent implements OnInit {
 
-	@Output addTodo: EventEmitter<any> = new EventEmitter();
+	@Output() addTodo: EventEmitter<any> = new EventEmitter();
 	title:string;
 
 
